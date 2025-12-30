d1422093bc0ed1fbe2a438d2745c64d40ec580b0
jteen-lab <jteen@jteen-lab.com>
2025-12-30 20:48:15 +0900

    fix(modules): Fix Alpine.js double initialization causing duplicate modules
    - Remove window variable deletion from x-init
    - Remove manual Alpine.initTree call (v3 auto-initializes)

--- services/republish/app/static/js/modules/list.js
+++ services/republish/app/static/js/modules/list.js
@@ -297,23 +297,6 @@ function moduleListApp() {
                     contentArea.innerHTML = formContent;
                     console.log('폼 내용 로드 완료');
 
-                    // Alpine.js 다시 초기화 (동적 컨텐츠를 위해)
-                    setTimeout(() => {
-                        if (typeof Alpine !== 'undefined') {
-                            console.log('Alpine.js 객체:', Alpine);
-                            if (Alpine.initTree) {
-                                Alpine.initTree(contentArea);
-                                console.log('Alpine.js initTree 완료');
-                            } else if (Alpine.start) {
-                                // Alpine.js v3에서는 start() 메소드 사용
-                                console.log('Alpine.js 다시 시작 시도...');
-                            } else {
-                                console.log('Alpine.js 버전이 다름. 수동 초기화 필요');
-                            }
-                        } else {
-                            console.warn('Alpine.js가 로드되지 않았습니다');
-                        }
-                    }, 50);
                 } else {
                     console.warn('모듈 폼 컨텐츠 엘리먼트를 찾을 수 없습니다');
                 }
@@ -455,7 +438,7 @@ function moduleListApp() {
             window[typeId] = moduleType;
 
             return `
-                <div x-data="moduleFormApp(window.${moduleId}, window.${typeId})" x-init="init(); delete window.${moduleId}; delete window.${typeId};">
+                <div x-data="moduleFormApp(window.${moduleId}, window.${typeId})" x-init="init()">
                     ${this.getFullFormTemplate()}
                 </div>
             `;
